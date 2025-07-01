e81b0a78d0972315b638b91c474be17398e47a6c
ExHansen <91862289+ExHansen@users.noreply.github.com>
2025-07-02 01:20:27 +0700

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -14,45 +14,44 @@ Usage:
 ===============================================================================
 */
 
--- =============================================================================
--- Create Dimension: gold.dim_customers
--- =============================================================================
-IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
-    DROP VIEW gold.dim_customers;
-GO
+-- ================================================================
+-- Drop views if they exist (PostgreSQL uses DROP VIEW IF EXISTS)
+-- ================================================================
 
-CREATE VIEW gold.dim_customers AS
+DROP VIEW IF EXISTS gold.fact_sales;
+DROP VIEW IF EXISTS gold.dim_products;
+DROP VIEW IF EXISTS gold.dim_customers;
+
+-- ================================================================
+-- Create Dimension View: gold.dim_customers
+-- ================================================================
+CREATE OR REPLACE VIEW gold.dim_customers AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
-    ci.cst_id                          AS customer_id,
-    ci.cst_key                         AS customer_number,
-    ci.cst_firstname                   AS first_name,
-    ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
+    ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id                              AS customer_id,
+    ci.cst_key                             AS customer_number,
+    ci.cst_firstname                       AS first_name,
+    ci.cst_lastname                        AS last_name,
+    la.cntry                               AS country,
+    ci.cst_marital_status                  AS marital_status,
     CASE 
-        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr 
-        ELSE COALESCE(ca.gen, 'n/a')  			   
-    END                                AS gender,
-    ca.bdate                           AS birthdate,
-    ci.cst_create_date                 AS create_date
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+        ELSE COALESCE(ca.gen, 'n/a')
+    END                                    AS gender,
+    ca.bdate                               AS birthdate,
+    ci.cst_create_date                     AS create_date
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
-GO
 
--- =============================================================================
--- Create Dimension: gold.dim_products
--- =============================================================================
-IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
-    DROP VIEW gold.dim_products;
-GO
-
-CREATE VIEW gold.dim_products AS
+-- ================================================================
+-- Create Dimension View: gold.dim_products
+-- ================================================================
+CREATE OR REPLACE VIEW gold.dim_products AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, 
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
     pn.prd_id       AS product_id,
     pn.prd_key      AS product_number,
     pn.prd_nm       AS product_name,
@@ -66,17 +65,12 @@ SELECT
 FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
-WHERE pn.prd_end_dt IS NULL; 
-GO
+WHERE pn.prd_end_dt IS NULL;
 
--- =============================================================================
--- Create Fact Table: gold.fact_sales
--- =============================================================================
-IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
-    DROP VIEW gold.fact_sales;
-GO
-
-CREATE VIEW gold.fact_sales AS
+-- ================================================================
+-- Create Fact View: gold.fact_sales
+-- ================================================================
+CREATE OR REPLACE VIEW gold.fact_sales AS
 SELECT
     sd.sls_ord_num  AS order_number,
     pr.product_key  AS product_key,
@@ -92,4 +86,3 @@ LEFT JOIN gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
-GO
